93929bd9288c05f87aef1fd8d2adbaf90b50288f
pokabu55 <okazumaru@gmail.com>
2023-03-31 22:43:22 +0900

    3ch 画像の生成

--- chap-03/test01.py
+++ chap-03/test01.py
@@ -4,6 +4,8 @@ import cv2
 width = 200
 height = 100
 value = 128
+channels = 3
+value3 = (0, 0, 255)  # BGR
 
 # 定義のwidth, height, 画素値=0 で埋めたグレースケール画像の作成
 img1 = np.zeros((height, width), np.uint8)
@@ -11,11 +13,17 @@ img1 = np.zeros((height, width), np.uint8)
 # 定義のwidth, height, 画素値=128 で埋めたグレースケール画像の作成
 img2 = np.full((height, width), value, np.uint8)
 
+# 3ch の真っ黒画像
+img3 = np.zeros((height, width, channels), np.uint8)
+
+# 3ch 真っ赤画像
+img4 = np.full((height, width, channels), value3, np.uint8)
+
 # 表示ウィンドウ
 cv2.imshow("img1", img1)
 cv2.imshow("img2", img2)
-
-img3 = np.zeros((height, width), np.uint8)
+cv2.imshow("img3", img3)
+cv2.imshow("img4", img4)
 
 # 何かキーを押すとウィンドウを閉じる
 cv2.waitKey(0)
